6193a35853ec2d042eef7034db22c37cb97ccafd
23120355@student.hcmus.edu.vn <23120355@student.hcmus.edu.vn>
2024-12-01 09:09:07 +0700

    b1_commit

--- test_git.cpp
+++ test_git.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+// Branch -1
 
 void first_program() {
 
     cout << "First Programming\n";
+}
+
+int findMax(vector<int>& a) {
+    int max = a[0];
+    for(int i = 1; i < a.size(); i++) {
+        if(a[i] > max) max = a[i];
+    }
+
+    return max;
 }
 
 int main() {
