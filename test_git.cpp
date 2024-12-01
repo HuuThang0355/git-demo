f6dc1e99a05a2f3caeacb9310d920d7ecf303f15
23120355@student.hcmus.edu.vn <23120355@student.hcmus.edu.vn>
2024-12-01 09:22:00 +0700

    sort function

--- test_git.cpp
+++ test_git.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 void first_program() {
 
     cout << "First Programming\n";
+}
+
+int sort(vector<int>& a) {
+    for(int i = 0; i < a.size() - 1; i++) {
+        for(int j = i + 1; j < a.size(); j++) {
+            if(a[i] > a[j])
+                swap(a[i], a[j]);
+        }
+    }
 }
 
 int main() {
